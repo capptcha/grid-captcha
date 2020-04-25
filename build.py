5b6703eb35e0bc17ef793ccacca155b6397829c1
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2020-04-25 22:04:42 +0530

    minor bug fixes

--- build.py
+++ build.py
@@ -2,15 +2,16 @@ from grid_captcha import CaptchaBuilder
 from tqdm import tqdm
 import os
 os.system('mkdir grid_captcha_data')
-
+counter = 0
 for i in tqdm(range(1000000)):
     builder = CaptchaBuilder()  # Create builder
     builder.build()  # Generate new image
     #print(''.join(builder.ret_name()))
     word = builder.word
     #builder.show()
-    builder.save(str(''.join(builder.ret_name())+".png"))
+    builder.save(str(''.join(builder.ret_name())+"_"+str(counter)+".png"))
     image_jpeg = builder.base64()
     image_png = builder.base64(image_format='PNG')
     os.system('mv *.png grid_captcha_data')
+    counter += 1
     
